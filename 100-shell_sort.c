b0ef4fe134eeb12510d3e5c19146c2d5668177af
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-25 03:13:56 -0700

    shell sort updated by adding swap function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - function that swap two integers
+ * @i: the first integer to be swapped
+ * @j: the second integer to be swapped
+ */
+void swap(int *i, int *j)
+{
+	int temp;
+
+	temp = *i;
+	*i = *j;
+	*j = temp;
+}
+
+/**
  * shell_sort - function that sorts an array of integers
  * in ascending order using the Shell sort
  * @array: the array of integers
@@ -10,27 +24,34 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	/*initializes gap to 1*/
 	size_t gap = 1;
-	int temp;
 	size_t i, j;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
+	if (array == NULL || size < 2)
+		return;
 
-	while (gap > 0)
+	/*this loop calculate gap and multiply it by 3 and add 1*/
+	while (gap < (size / 3))
 	{
-		for (i = gap; i < size; i++)
-		{
-			temp = array[i];
-			j = i;
+		gap = gap * 3 + 1;
+	}
 
-			while (j >= gap && array[j - gap] > temp)
+	while (gap >= 1)
+	{
+		i = gap;
+		while (i < size)
+		{
+			j = i;
+			/* this loop compared and swapped the elemnts*/
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				array[j] = array[j - gap];
+				swap(array + j, array + (j - gap));
 				j -= gap;
 			}
-			array[j] = temp;
+			i++;
 		}
+		print_array(array, size);
 		gap /= 3;
 	}
 }
